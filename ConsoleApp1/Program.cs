54cfe8d8a985766658c55d8486c4f0ea684860f6
harsimrangill-dev <parthsharma12341@gmail.com>
2023-02-13 22:56:45 +0000

    this is the complete task1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace task1
+{
+    class program
+    {
+        static void Main(string[] args) 
+        {
+
+            var codes = File.ReadAllLines("code.txt");
+            var lines = new List<string>(codes);
+            //methods
+
+            foreach ( string line in lines)
+            {
+                char[] array;
+                array = new char[12];
+                for (int i = 0; i < 12; i++)
+                {
+                    array[i] = line[i];
+                }
+                Array.Sort(array);
+                int c = 1;
+                for (int i = 0; i < 11; i++)
+                {
+                    if (array[i] == array[i + 1])
+                    {
+                        c++;
+                    }
+                    else
+                    {
+                        Console.Write(Convert.ToString(array[i]));
+                        Console.WriteLine("  :  " + c);
+                        c = 1;
+                    }
+                }
+                Console.WriteLine("\n\n\n");
+
+
+            }
+        }
+    }
+}
